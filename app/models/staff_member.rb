905a78a0db9451e47f69936f735408a3cc03fa35
9kuwa0 <techkuwa@gmail.com>
2024-11-09 12:51:15 +0900

    スタッフモデル作成

---
+++ app/models/staff_member.rb
@@ -0,0 +1,20 @@
+class StaffMember < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  # has_many :patients
+  
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :staff_department
+  belongs_to :staff_affiliation
+
+  with_options presence: true do
+    validates :staff_number, format: { with: /\A\d{4}\z/ }
+    validates :staff_last_name
+    validates :staff_first_name
+    validates :staff_department_id, numericality: { other_than: 1 }
+    validates :staff_affiliation_id, numericality: { other_than: 1 }
+  end
+end
