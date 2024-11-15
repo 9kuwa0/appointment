af0f88ca4e7d777345149abe7a0da80a89b0428c
9kuwa0 <techkuwa@gmail.com>
2024-11-15 16:17:57 +0900

    ログアウトを可能にした

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :staff_members, controllers: { registrations: 'staff_members/registrations' }
   root to: 'home#index'
 
+  resources :staff_members, only: [:destroy]
+
 end
