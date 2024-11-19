82227c02082e20d098fb31d05195eb8a2e7d3655
9kuwa0 <techkuwa@gmail.com>
2024-11-19 13:55:49 +0900

    患者登録ができる

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -4,5 +4,19 @@ class PatientsController < ApplicationController
     @patient = Patient.new
   end
 
-  
+  def create
+    @patient = Patient.new(patient_params)
+    if @patient.save
+      redirect_to staff_member_index_path, notice: "登録しました"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+
+  private
+
+  def patient_params
+    params.require(:patient).permit(:patient_number, :patient_last_name, :patient_first_name, :patient_last_name_kana, :patient_first_name_kana, :birthday, :floor, :room)
+  end
 end
