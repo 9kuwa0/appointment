42ff1760eeffe5df0f3a4ae25f0f6ba5c287301e
9kuwa0 <techkuwa@gmail.com>
2024-11-19 11:41:21 +0900

    patientバリデート作成

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,2 +1,13 @@
 class Patient < ApplicationRecord
+
+  belongs_to :staff_member
+
+  with_options presence: true do
+    validates :ptient_number
+    validates :ptient_last_name
+    validates :ptient_first_name
+    validates :ptient_last_name_kana, format: { with: /\A[ァ-ヶー-]+\z/}
+    validates :ptient_first_name_kana, format: { with: /\A[ァ-ヶー-]+\z/}
+    validates :birthday
+  end
 end
