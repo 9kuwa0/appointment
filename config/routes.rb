72a8dfe22dde9c055bc7e94cba8aa21424cd9fb7
9kuwa0 <techkuwa@gmail.com>
2024-11-19 14:50:40 +0900

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   root to: 'home#index'
 
-  resources :staff_members, only: [:destroy]
+  resources :staff_members, only: [:index]
   resources :patients, only: [:new, :create]
 
 
