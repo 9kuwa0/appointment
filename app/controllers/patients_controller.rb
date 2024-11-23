9db4c53f6d70fee257f83aaa30e5782b983c8e54
9kuwa0 <techkuwa@gmail.com>
2024-11-23 20:07:44 +0900

    コントローラー定義

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,5 +1,7 @@
 class PatientsController < ApplicationController
 
+  before_action :set_patient, only: [:show, :edit]
+
   def new
     @patient = Patient.new
   end
@@ -14,12 +16,19 @@ class PatientsController < ApplicationController
   end
 
   def show
-    @patient = Patient.find(params[:id])
+
+  end
+
+  def edit
   end
 
   private
 
   def patient_params
     params.require(:patient).permit(:patient_number, :patient_last_name, :patient_first_name, :patient_last_name_kana, :patient_first_name_kana, :birthday, :floor, :room)
+  end
+
+  def set_patient
+    @patient = Patient.find(params[:id])
   end
 end
