70d4fc13cea5b6cc0713edeefc5f91cf31ac2c95
9kuwa0 <techkuwa@gmail.com>
2024-12-01 10:12:15 +0900

    スタッフ用シンプルカレンダー作成

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def display_status(day, time)
+    if family_signed_in?
+      Promise.status_for(day, time)
+    elsif staff_member_signed_in?
+      promises = Promise.where(day: day, meeting_time_id: MeetingTime.find_by(name: time).id)
+      if promises.exists?
+        promises.map do |f|
+           "#{f.patient.patient_last_name} #{f.patient.patient_first_name} - #{f.family.family_last_name} #{f.family.family_first_name}"
+        end.join(", ")
+      else
+        "空き"
+      end
+    else
+    end
+  end
 end
