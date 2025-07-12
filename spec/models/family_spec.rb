019ff222bd0cd64b51c8afe8c7bf2dc0218e1839
9kuwa0 <techkuwa@gmail.com>
2025-07-12 17:03:01 +0900

    患者登録用のエグザンプル用意

--- spec/models/family_spec.rb
+++ spec/models/family_spec.rb
@@ -1,5 +1,58 @@
 require 'rails_helper'
 
 RSpec.describe Family, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @family = FactoryBot.build(:family)
+  end
+
+  describe '患者家族新規登録' do
+      # 患者家族登録正常系
+    context '登録できる場合' do
+      it 'すべての情報が正しく入力されていれば登録できる' do
+      end
+    end
+      # 患者家族登録異常系
+    context '登録できない場合' do
+        # 姓
+      it '姓が空では登録できない' do
+      end
+        # 名
+      it '名が空では登録できない' do
+      end
+        # セイ
+      it 'セイが空では登録できない' do
+      end
+      it 'セイが平仮名では登録できない' do
+      end
+      it 'セイが半角カタカナでは登録できない' do
+      end
+      it 'セイが漢字では登録できない' do
+      end
+      it 'セイがアルファベットでは登録できない' do
+      end
+      # メイ
+      it 'メイが空では登録できない' do
+      end
+      it 'メイが平仮名では登録できない' do
+      end
+      it 'メイが半角カタカナでは登録できない' do
+      end
+      it 'メイが漢字では登録できない' do
+      end
+      it 'メイがアルファベットでは登録できない' do
+      end
+        # メール
+      it 'メールが空では登録できない' do
+      end
+      it '重複したメールアドレスが存在する場合登録できない' do
+      end
+        # パスワード
+      it 'パスワードが空では登録できない' do
+      end
+      it 'パスワードが6文字未満では登録できない' do
+      end
+      it 'パスワードとパスワード確認が一致しないと登録できない' do
+      end
+    end
+  end
 end
