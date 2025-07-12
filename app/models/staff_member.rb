e2916d50e6f6e4589648862b96373030acc193c1
9kuwa0 <techkuwa@gmail.com>
2025-07-12 12:38:39 +0900

    スタッフログインページのエラーを日本語化

--- app/models/staff_member.rb
+++ app/models/staff_member.rb
@@ -11,10 +11,10 @@ class StaffMember < ApplicationRecord
   belongs_to :staff_affiliation
 
   with_options presence: true do
-    validates :staff_number, format: { with: /\A\d{4}\z/ }
+    validates :staff_number, format: { with: /\A\d{4}\z/, message: "は4桁の数字で入力してください" }
     validates :staff_last_name
     validates :staff_first_name
-    validates :staff_department_id, numericality: { other_than: 1 }
-    validates :staff_affiliation_id, numericality: { other_than: 1 }
+    validates :staff_department_id, numericality: { other_than: 1, message: "は未選択以外で入力してください" }
+    validates :staff_affiliation_id, numericality: { other_than: 1, message: "は未選択以外で入力してください" }
   end
 end
